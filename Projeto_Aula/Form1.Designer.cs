e8480927d58354e3bc07fc84b8db5daf80f0d9ac
professor-vanderson <vanderson.bossi@hotmail.com>
2021-02-01 10:59:54 -0300

    Aula15

---
+++ Projeto_Aula/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace Projeto_Aula
+{
+    partial class frmAula15
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEndereco = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.btnGravar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(78, 16);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(295, 20);
+            this.txtNome.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Endereço:";
+            // 
+            // txtEndereco
+            // 
+            this.txtEndereco.Location = new System.Drawing.Point(78, 55);
+            this.txtEndereco.Name = "txtEndereco";
+            this.txtEndereco.Size = new System.Drawing.Size(366, 20);
+            this.txtEndereco.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Telefone:";
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Location = new System.Drawing.Point(78, 91);
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(141, 20);
+            this.txtTelefone.TabIndex = 5;
+            // 
+            // btnGravar
+            // 
+            this.btnGravar.Location = new System.Drawing.Point(15, 146);
+            this.btnGravar.Name = "btnGravar";
+            this.btnGravar.Size = new System.Drawing.Size(75, 23);
+            this.btnGravar.TabIndex = 6;
+            this.btnGravar.Text = "Gravar";
+            this.btnGravar.UseVisualStyleBackColor = true;
+            this.btnGravar.Click += new System.EventHandler(this.btnGravar_Click);
+            // 
+            // frmAula15
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 409);
+            this.Controls.Add(this.btnGravar);
+            this.Controls.Add(this.txtTelefone);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtEndereco);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label1);
+            this.Name = "frmAula15";
+            this.Text = "Aula 15";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEndereco;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTelefone;
+        private System.Windows.Forms.Button btnGravar;
+    }
+}
+
---
+++ Projeto_Aula/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Projeto_Aula
+{
+    public partial class frmAula15 : Form
+    {
+        public frmAula15()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGravar_Click(object sender, EventArgs e)
+        {
+            //Abrir o banco de dados 
+            Conectar();
+
+            //inserir as informações do form
+           
+            
+            //fechar o banco de dados
+
+            //exibir uma mensagem(ok)
+        }
+        public void Conectar()
+        {
+            try
+            {
+                string conectionString = "Data Source=.\\SQLEXPRESS;" +
+                    "Initial Catalog= Aula15;" +
+                    "User ID=sa;" +
+                    "Password=1425";
+                SqlConnection sqlConect = new SqlConnection(conectionString);
+                sqlConect.Open();
+              
+                //inserir informações
+                SqlCommand com = new SqlCommand(@"insert into Contatos(Nome,Endereco,Telefone) " +
+        "Values(@nome,@endereco,@telefone)", sqlConect);
+
+                //com.CommandText = @"insert into Contatos(Nome,Endereco,Telefone) " +
+        //"Values(@nome,@endereco,@telefone)";
+
+                com.Parameters.AddWithValue("@nome", txtNome.Text);
+                com.Parameters.AddWithValue("@endereco", txtEndereco.Text);
+                com.Parameters.AddWithValue("@telefone", txtTelefone.Text);
+                com.ExecuteNonQuery();
+                MessageBox.Show("Dados Salvos com sucesso!");
+
+
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
